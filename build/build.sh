efb844b8473256cc779582dad1a85570dbc8e2b0
Corey McCormick <624784+SimplyCorey@users.noreply.github.com>
2025-07-22 09:14:56 -0400

    Upgrade to Node22, resolve buffer overflow issue, sign MacOS arm64 binary.

--- build/build.sh
+++ build/build.sh
@@ -6,11 +6,18 @@ targets=("darwin-x64" "darwin-arm64" "linux-x64" "linux-arm64")
 for target in "${targets[@]}"
 do
   echo "Building: $target"
-  npx pkg --target=node20-"$target" ./build/mjml.js --compress=Brotli --output=./bin/mjml-"$target"
+  npx pkg --target=node22-"$target" ./build/mjml.js --compress=Brotli --output=./bin/mjml-"$target"
 done
 
-echo "Uploading to DigitalOcean Spaces"
-s3cmd --host https://nyc3.digitaloceanspaces.com --host-bucket "%(bucket)s.nyc3.digitaloceanspaces.com" --acl-public --access_key "$SPACES_KEY" --secret_key "$SPACES_SECRET" sync ./bin/mjml* s3://defectivecode-packages/packages/mjml/"$VERSION"/
+echo "Signing MacOS ARM64 binary."
+macos_arm64_binary="./bin/mjml-darwin-arm64"
+codesign --remove-signature "$macos_arm64_binary"
+codesign --sign "$MAC_DEVELOPER_KEY" --timestamp --options runtime --entitlements ./build/entitlements.plist "$macos_arm64_binary"
+zip "$macos_arm64_binary".zip "$macos_arm64_binary"
+xcrun notarytool submit "$macos_arm64_binary".zip --keychain-profile "$MACOS_NOTARIZATION" --wait
 
-echo "Cleaning up"
-find ./bin ! -name '.gitkeep' -type f -delete
+echo "Uploading to DigitalOcean Spaces"
+s3cmd --host https://nyc3.digitaloceanspaces.com --host-bucket "defectivecode-packages.nyc3.digitaloceanspaces.com" --acl-public --access_key "$SPACES_KEY" --secret_key "$SPACES_SECRET" sync ./bin/mjml* s3://defectivecode-packages/packages/mjml/"$VERSION"/
+
+#echo "Cleaning up"
+#find ./bin ! -name '.gitkeep' -type f -delete
