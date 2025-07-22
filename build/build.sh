9a65d0d179f4dda234a28d39b0f870709e046d10
Corey McCormick <624784+SimplyCorey@users.noreply.github.com>
2025-07-22 10:41:52 -0400

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -6,7 +6,7 @@ targets=("darwin-x64" "darwin-arm64" "linux-x64" "linux-arm64")
 for target in "${targets[@]}"
 do
   echo "Building: $target"
-  npx pkg --target=node22-"$target" ./build/mjml.js --compress=Brotli --output=./bin/mjml-"$target"
+  npx pkg --target=node20-"$target" ./build/mjml.js --compress=Brotli --output=./bin/mjml-"$target"
 done
 
 echo "Signing MacOS ARM64 binary."
@@ -15,9 +15,10 @@ codesign --remove-signature "$macos_arm64_binary"
 codesign --sign "$MAC_DEVELOPER_KEY" --timestamp --options runtime --entitlements ./build/entitlements.plist "$macos_arm64_binary"
 zip "$macos_arm64_binary".zip "$macos_arm64_binary"
 xcrun notarytool submit "$macos_arm64_binary".zip --keychain-profile "$MACOS_NOTARIZATION" --wait
+rm "$macos_arm64_binary".zip
 
 echo "Uploading to DigitalOcean Spaces"
 s3cmd --host https://nyc3.digitaloceanspaces.com --host-bucket "defectivecode-packages.nyc3.digitaloceanspaces.com" --acl-public --access_key "$SPACES_KEY" --secret_key "$SPACES_SECRET" sync ./bin/mjml* s3://defectivecode-packages/packages/mjml/"$VERSION"/
 
-#echo "Cleaning up"
-#find ./bin ! -name '.gitkeep' -type f -delete
+echo "Cleaning up"
+find ./bin ! -name '.gitkeep' -type f -delete
